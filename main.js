2e04a946e768da07b0e30101d7a0fb51fd2094c5
Josh Curtiss <J.Curtiss@RockValleyCollege.edu>
2017-02-28 10:36:29 -0600

    Disable developer tools.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {app, BrowserWindow} = electron;
 app.on("ready", () => {
     var videoWin=new BrowserWindow({width:900, height:600, frame:false, fullscreen:true});
     videoWin.loadURL(`file://${__dirname}/views/index.html`);
-    videoWin.webContents.openDevTools();   // Opening the dev tools
+    //videoWin.webContents.openDevTools();   // Opening the dev tools
 });
 
 exports.quit=()=>{
