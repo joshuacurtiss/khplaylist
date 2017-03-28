3b01099817e5232b2041ae22d42444a6438ef179
Josh Curtiss <J.Curtiss@RockValleyCollege.edu>
2017-03-28 15:15:16 -0500

    Update Reference Video code to work with new theoreference changes.
    Haven't actually tested this code out yet, but it's also not actively used yet, so I'm committing.

--- model/ReferenceVideo.js
+++ model/ReferenceVideo.js
@@ -7,7 +7,9 @@ class ReferenceVideo {
     constructor(reference,path,webvtt) {
         this.path=path;
         this.webvtt=webvtt;
-        this.reference=reference; // Must be set after webvtt is set.
+        // Pass cues to reference after webvtt is set.
+        reference.availableCues=this.webvtt.data;
+        this.reference=reference;
         return this;
     }
 
@@ -15,7 +17,8 @@ class ReferenceVideo {
 
     get webvtt() {return this._webvtt}
     set webvtt(webvtt) {
-        this._webvtt=new WebVTT(webvtt);
+        if( webvtt instanceof WebVTT ) this._webvtt=webvtt;
+        else this._webvtt=new WebVTT(webvtt);
     }
 
     get reference() {return this._reference}
@@ -23,45 +26,29 @@ class ReferenceVideo {
         this._reference=reference;
         if( reference.valid() ) {
             var list=[];
-            var cues;
-            // TODO: Fine-tune this logic to work with citations other than pars
-            for( var p of reference.pars ) {
-                cues=this.getCuesByPar(p);
-                for( var cue of cues ) {
-                    if( list.length && list[list.length-1].end==cue.start ) {
-                        var last=list[list.length-1];
-                        last.end=cue.end;
-                        last.content=last.content.split("-")[0]+"-"+cue.content;
-                    } else {
-                        list.push({start:cue.start,end:cue.end,content:cue.content});
-                    }
+            var cue;
+            for( var cuename of reference.cues ) {
+                cue=this.getCueByName(cuename);
+                if( list.length && list[list.length-1].end==cue.start ) {
+                    var last=list[list.length-1];
+                    last.end=cue.end;
+                    last.content=last.content.split("-")[0]+"-"+cue.content;
+                } else {
+                    list.push({start:cue.start,end:cue.end,content:cue.content});
                 }
             }
             this.list=list;
         }
     }
 
-    getCuesByPar(par) {
-        var parRegex=new RegExp(`^p ${par}[a-z]{0,1}$`,`i`);
-        var parExtrasRegex=/^(p|q|r|subheading|box|art)\s/i;
-        var match, index, first, last;
-        // Find the first par match
-        last=first=index=this.webvtt.data.findIndex((c)=>{return parRegex.test(c.content)});
-        // Find the last par match (after the first one)
-        while( index>=0 ) {
-            index=this.webvtt.data.findIndex((c,idx)=>{return (idx>index && parRegex.test(c.content))});
-            if( index>=0 ) last=index;
-        }
-        // Now check if any other materials after the par should be included
-        index=last;
-        while( index>=0 ) {
-            if( index+1<this.webvtt.data.length && !parExtrasRegex.test(this.webvtt.data[index+1].content)) last=++index;
-            else index=-1;
-        }
-        // Push out the matching cues
-        var cues=[];
-        for( index=first ; index<=last ; index++ ) cues.push(this.webvtt.data[index]);
-        return cues;
+    calcPlayLength() {
+        var sum=0;
+        for( var cue of this.list ) sum+=cue.end-cue.start;
+        return sum;
+    }
+
+    getCueByName(cuename) {
+        return cue=this.webvtt.data.find(item=>item.content.toLowerCase()==cuename.toLowerCase());
     }
 }
 
--- model/ReferenceVideoUtil.js
+++ model/ReferenceVideoUtil.js
@@ -1,11 +1,17 @@
+let Reference=require("../bower_components/theoreference/Reference");
 let ReferenceVideo=require("./ReferenceVideo");
+let WrapperController=require("../bower_components/webvtt/wrappers/WrapperController");
 let fs=require("fs-extra");
+let path=require("path");
+let escapeStringRegexp=require("escape-string-regexp");
 
 class ReferenceVideoUtil {
 
     constructor(videopath) {
         // Video path is the path under which all video files are housed. Subdirectories ok.
         this.videopath=videopath;
+        this.videoAppController=new WrapperController();
+        this.videoApp=this.videoAppController.wrapper;
         return this;
     }
 
@@ -13,10 +19,13 @@ class ReferenceVideoUtil {
     set videopath(path) {
         this._videopath=path||"";
         // Keep a list of all the videos
-        var pathwalk=fs.walkSync(this.videopath);
+        var pathwalk=[];
+        try {
+            pathwalk=fs.walkSync(this.videopath);
+        } catch(err) {}
         var videos=[], webvtts=[], f, ext;
-        for( var path of pathwalk ) {
-            f=path.split("/").pop();
+        for( var p of pathwalk ) {
+            f=p.split(path.sep).pop();
             ext=f.split(".").pop().toLowerCase();
             if( ReferenceUtil.VIDEOEXT.indexOf(ext)>=0 ) videos.push(path);
             else if( ReferenceUtil.WEBVTTEXT.indexOf(ext)>=0 ) webvtts.push(path);
@@ -26,30 +35,54 @@ class ReferenceVideoUtil {
     }
 
     /*
-     *  createVideo: Receives a publication and returns a ScriptureVideo object
-     *  that knows how to exactly play that scripture from the video.
+     *  createVideo: Receives a reference and returns a ReferenceVideo object
+     *  that knows how to exactly play that reference from the video.
      * 
      */
-    createVideo(reference) {
+    createVideo(reference,cb) {
         // RegEx: /BOOKSYMBOL_something_CHAPTER_r999p.ext
         if( reference.valid() ) {
             var chapter="0*"+reference.chapter.toString();
             var baseFileRegex=
-                "\/"+
+                escapeStringRegexp(path.sep)+
                 reference.publication.symbol+"_"+
                 "\\w+_"+
                 chapter+"_"+
                 "r\\d{3}p\\.\\w{3}";
             var videoRegex=new RegExp(baseFileRegex+"$","i");
             var webvttRegex=new RegExp(baseFileRegex+"\\.\\w{3,6}$","i");
-            var videoFile=this.videos.find((value)=>{return videoRegex.test(value)});
-            var webvttFile=this.webvtts.find((value)=>{return webvttRegex.test(value)});
-            // TODO: Handle if file doesn't exist, create webvtt on the fly.
-            var webvtt="";
-            try {
-                webvtt=fs.readFileSync(webvttFile,"UTF-8");
-            } catch (error) {}
-            return new ReferenceVideo(reference,videoFile,webvtt);
+            var videoFile=this.videos.find(value=>videoRegex.test(value));
+            var webvttFile=this.webvtts.find(value=>webvttRegex.test(value));
+            if( !videoFile ) {
+                // No video file. Report the error.
+                console.log(`No video file for ${reference.toString()}.`);
+                cb({code:"novideo",tag:"Not Found",message:"Video file could not be found!"},new ReferenceVideo(reference));
+            } else if( !webvttFile ) {
+                // If no webvtt file, then create one and send stuff.
+                console.log(`No webvtt file for ${videoFile}. Make one now.`);
+                if( this.videoApp ) {
+                    this.videoApp.createWebVTT(videoFile,(err,webvtt)=>{
+                        if( err ) {
+                            console.error(err.toString());
+                            cb({code:"indexerr",tag:"Index Error",message:err.toString()},new ReferenceVideo(reference,videoFile));
+                        } else {
+                            var webvttpath=videoFile+".webvtt";
+                            this.webvtts.push(webvttpath);
+                            fs.writeFileSync(webvttpath, webvtt.toString());
+                            console.log(`Created ${webvttpath}!`);
+                            cb(null,new ReferenceVideo(reference,videoFile,webvtt));
+                        } 
+                    });
+                } else {
+                    cb({code:"indexerr",tag:"Index Error",message:"No application available to index video!"},new ReferenceVideo(reference,videoFile));
+                }
+            } else {
+                // All is well. Read the webvtt file and send stuff.
+                fs.readFile(webvttFile,{encoding:"UTF-8"},(err,webvtt)=>{
+                    console.log(`Read ${webvttFile}.`);
+                    cb(null,new ReferenceVideo(reference,videoFile,webvtt));
+                });
+            }
         }
     }
 
