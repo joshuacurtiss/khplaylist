5ba873aa25bc4a9b3f69c44f89082ad368fe7c10
Josh Curtiss <git@curtiss.me>
2017-11-25 22:32:59 -0600

    Don't pass width/height for fullscreen window.
    There's no point to it. It immediately goes fullscreen anyway.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const {app, BrowserWindow} = electron;
 
 app.on("ready", () => {
-    var videoWin=new BrowserWindow({width:900, height:600, frame:false, fullscreen:true});
+    var videoWin=new BrowserWindow({frame:false, fullscreen:true});
     videoWin.loadURL(`file://${__dirname}/views/index.html`);
     exports.win=videoWin;
     //videoWin.webContents.openDevTools();   // Opening the dev tools
